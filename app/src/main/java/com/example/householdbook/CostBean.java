60b4c7049dc073560ce8194daf1311ea189697b8
YourDeepEnd <1725743751@qq.com>
2020-02-13 13:15:09 +0800

    Initial commit

---
+++ app/src/main/java/com/example/householdbook/CostBean.java
@@ -0,0 +1,13 @@
+package com.example.householdbook;
+
+/**
+ * Created by yons on 2017/8/20.
+ */
+
+public class CostBean {
+    public String costTitle;
+    public String costDate;
+    public String costMoney;
+
+
+}
